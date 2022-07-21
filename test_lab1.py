c50d489b9ea3e310f7df547f8ce110ecc2748c51
EC2 Default User <ec2-user@ip-172-31-71-119.ec2.internal>
2022-07-21 02:23:36 +0000

    test_lab1.py added

---
+++ test_lab1.py
@@ -0,0 +1,12 @@
+def increment_by_one(i):
+    return i+1
+    
+def decrement_by_one(i):
+    return i-1
+    
+
+def test_answer():
+    assert increment_by_one(1) == 2
+    assert decrement_by_one(1) == 0
+
+    
